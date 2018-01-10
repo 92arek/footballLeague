ea566b4d328c8522ea48f85e760697c264a4c587
Arek Pamulski <a.pamulski@gmail.com>
2018-01-10 19:36:58 +0100

    utworzenie equalsa, hasCode'a, toStringa i getterów

--- src/main/java/footballTeam/Player.java
+++ src/main/java/footballTeam/Player.java
@@ -1,5 +1,7 @@
 package footballTeam;
 
+import java.util.Objects;
+
 public class Player {
 
     private final String name;
@@ -8,5 +10,35 @@ public class Player {
     public Player(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name) &&
+                Objects.equals(surname, player.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
--- src/main/java/footballTeam/Team.java
+++ src/main/java/footballTeam/Team.java
@@ -1,10 +1,26 @@
 package footballTeam;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Team {
 
     private final String teamName;
+    private final Set<Player> players = new HashSet<>();
 
     public Team(String teamName) {
         this.teamName = teamName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
     }
 }
