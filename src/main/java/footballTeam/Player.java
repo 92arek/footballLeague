f03d8d9e820331f235cacad79d2cdd1d62f52254
Arek Pamulski <a.pamulski@gmail.com>
2018-01-10 18:44:58 +0100

    utworzono klasy Player i Team

---
+++ src/main/java/footballTeam/Player.java
@@ -0,0 +1,13 @@
+package footballTeam;
+
+public class Player {
+
+    private final String name;
+    private final String surname;
+
+
+    public Player(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
---
+++ src/main/java/footballTeam/Team.java
@@ -0,0 +1,4 @@
+package footballTeam;
+
+public class Team {
+}
